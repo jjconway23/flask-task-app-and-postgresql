87fa557145e36d9156efbb1b3ccb0b5e47e4e546
jjconway23 <jacob.peat01@gmail.com>
2022-08-04 11:41:57 +0000

    add tasks page and functionality to add a new task

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
+
     let sideNav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sideNav);
-  });
+  
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: 'dd, mmm, yyyy',
+      i18n: {done:"Select"}
+    });
+  
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
