57a2b48e078d3bb9426820e2fb655cedad4f9b48
jjconway23 <jacob.peat01@gmail.com>
2022-08-04 12:02:03 +0000

    Add functionality to view tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -10,3 +10,6 @@
   
     let selects = document.querySelectorAll('select');
     M.FormSelect.init(selects);
+
+    let collapsibles = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsibles);
